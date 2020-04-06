208f911accd3d6e6034cad6800eaa2358be1788e
savsir <svsrcnkt@gmail.com>
2020-04-06 23:09:15 +0300

    cepheus: Import NoCutoutOverlay from OOS (dev. options)
    Thanks to raystef66

--- sm8150.mk
+++ sm8150.mk
@@ -408,6 +408,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version.xml
 
+# NoCutout
+PRODUCT_PACKAGES += \
+    NoCutoutOverlay
+	
 # Wallpaper
 PRODUCT_PACKAGES += \
     PixelLiveWallpaperPrebuilt
